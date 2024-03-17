1ce151573797e21f638dfe3d03f4de2a09c1c953
MartinMF88 <martin.martinez.fontoura@gmail.com>
2024-03-17 15:20:51 -0500

    modificaciones-modulo

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import os
-from csv_logic import write_csv, load_csv_to_db
+from csv_logic import write_csv
 from models import Department, HiredEmployee, Job
 from database import initialize_db
 
@@ -17,8 +17,7 @@ def upload_hired_employees_csv():
     try:
         hired_employees_file = request.files['hired_employees']
         file_path = write_csv(hired_employees_file, hired_employees_file.filename)
-        load_csv_to_db(file_path, HiredEmployee)
-        return jsonify({'message': 'Hired employees CSV uploaded and data loaded to database successfully'}), 200
+        return jsonify({'message': 'Hired employees CSV uploaded successfully'}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
@@ -27,8 +26,7 @@ def upload_departments_csv():
     try:
         departments_file = request.files['departments']
         file_path = write_csv(departments_file, departments_file.filename)
-        load_csv_to_db(file_path, Department)
-        return jsonify({'message': 'Departments CSV uploaded and data loaded to database successfully'}), 200
+        return jsonify({'message': 'Departments CSV uploaded successfully'}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
@@ -37,8 +35,7 @@ def upload_jobs_csv():
     try:
         jobs_file = request.files['jobs']
         file_path = write_csv(jobs_file, jobs_file.filename)
-        load_csv_to_db(file_path, Job)
-        return jsonify({'message': 'Jobs CSV uploaded and data loaded to database successfully'}), 200
+        return jsonify({'message': 'Jobs CSV uploaded successfully'}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
